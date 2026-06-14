a408668a778a8ac23d2205ed26206a3b5ee14b28
abdulhadi240 <ah912425@gmail.com>
2026-06-14 13:51:24 +0500

    rag enpoint udpated

--- app/tools.py
+++ app/tools.py
@@ -103,7 +103,7 @@ def rag(query: str , reference: str) -> Dict[str, Any]:
     payload = {
         "query": query.strip(),
         "reference":reference,
-        "top_k" : 20
+        "top_k" : 10
 
     }
     
